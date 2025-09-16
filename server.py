0bfea1832c78dc102283178d1117c79507eedaa9
davidcastro31 <davidguille31@gmail.com>
2025-09-16 07:58:33 -0600

    Enviar datos desde cliente a servidor

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from urllib import parse
+
 port = 3000
 
 class miServidor(SimpleHTTPRequestHandler):
@@ -6,6 +8,18 @@ class miServidor(SimpleHTTPRequestHandler):
         if self.path=="/":
             self.path="/index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
+        
+    def do_POST(self):
+        longitud = int(self.headers['Content-Length'])
+        datos = self.rfile.read(longitud)
+        datos = datos.decode("utf-8")
+        datos = parse.unquote(datos)
+        
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(datos.encode("utf-8"))
+        
+        
 print("servidor ejecutandose en el puerto 80", port)
 server= HTTPServer(("localhost", port), miServidor)
 server.serve_forever()
