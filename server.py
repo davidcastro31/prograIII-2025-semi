3e6fa1588718bf3d7874d791450f8ba592407689
davidcastro31 <davidguille31@gmail.com>
2025-09-29 22:12:11 -0600

    terminacion del CRUD alumnos

--- server.py
+++ server.py
@@ -12,6 +12,11 @@ class miServidor(SimpleHTTPRequestHandler):
         if self.path=="/":
             self.path="index.html"
             return SimpleHTTPRequestHandler.do_GET(self)
+        if self.path=="/alumnos":
+            alumnos = crudAlumno.consultar("")
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(alumnos).encode('utf-8'))
     
     def do_POST(self):
         longitud = int(self.headers['Content-Length'])
